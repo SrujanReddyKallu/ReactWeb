250de8edd1125c591469ce31c66d856dbbcaaeb1
SrujanReddyKallu <srujankmit@gmail.com>
2023-10-19 09:48:09 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,12 @@ function App() {
     return (
         <HashRouter basename='/'>
             <Routes>
-                <Route path="/ReactWeb" element={<Example/>}/>
-                <Route path="/ReactWeb/signup" element={<Register/>}/>
-                <Route path="/ReactWeb/login" element={<Login/>}/>
-                <Route path="/ReactWeb/jugad" element={<Example/>}/>
-                <Route path="/ReactWeb/dashboard" element={<Dashboard/>}/>
-                <Route path="/ReactWeb/project1" element={<Project1/>}/>
+                <Route path="/" element={<Example/>}/>
+                <Route path="/signup" element={<Register/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/jugad" element={<Example/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/project1" element={<Project1/>}/>
             </Routes>
         </HashRouter>
     );
